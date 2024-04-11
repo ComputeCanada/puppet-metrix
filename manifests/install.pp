4c182214cd207e136aad0d6f90eef40d3205e5f1
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2024-04-11 14:02:06 -0400

    Move each class in its own file

---
+++ manifests/install.pp
@@ -0,0 +1,75 @@
+class profile::userportal::install (String $version) {
+  ensure_packages(['python38', 'python38-devel'])
+  ensure_packages(['openldap-devel', 'gcc', 'mariadb-devel'])
+
+  # Using python3.8 with gunicorn
+  exec { 'userportal_venv':
+    command => '/usr/bin/python3.8 -m venv /opt/software/userportal-env',
+    creates => '/opt/software/userportal-env',
+    require => Package['python38'],
+  }
+
+  exec { 'userportal_upgrade_pip':
+    command     => 'pip3 install --upgrade pip',
+    path        => [
+      '/opt/software/userportal-env/bin',
+      '/usr/bin',
+    ],
+    refreshonly => true,
+    subscribe   => [
+      Exec['userportal_venv'],
+    ],
+  }
+
+  file { '/var/www/userportal/':
+    ensure => 'directory',
+    owner  => 'apache',
+    group  => 'apache',
+  }
+  -> archive { 'userportal':
+    ensure          => present,
+    source          => "https://github.com/guilbaults/TrailblazingTurtle/archive/refs/tags/v${version}.tar.gz",
+    creates         => '/var/www/userportal/manage.py',
+    path            => '/tmp/userportal.tar.gz',
+    extract         => true,
+    extract_path    => '/var/www/userportal/',
+    extract_command => 'tar xfz %s --strip-components=1',
+    cleanup         => true,
+    user            => 'apache',
+    notify          => [Service['httpd'], Service['gunicorn-userportal']],
+  }
+
+  exec { 'userportal_pip':
+    command     => 'pip3 install -r /var/www/userportal/requirements.txt',
+    path        => [
+      '/opt/software/userportal-env/bin',
+      '/usr/bin',
+    ],
+    refreshonly => true,
+    subscribe   => [
+      Archive['userportal'],
+      Exec['userportal_venv'],
+    ],
+    require     => [
+      Exec['userportal_venv'],
+      Exec['userportal_upgrade_pip'],
+      Package['python38-devel'],
+      Package['mariadb-devel'],
+      Package['openldap-devel'],
+      Package['gcc'],
+    ],
+  }
+
+  exec { 'pip install django-pam':
+    command => 'pip3 install django-pam',
+    path    => [
+      '/opt/software/userportal-env/bin',
+      '/usr/bin',
+    ],
+    creates => '/opt/software/userportal-env/lib/python3.8/site-packages/django_pam/__init__.py',
+    require => [
+      Exec['userportal_venv'],
+      Exec['userportal_upgrade_pip'],
+    ],
+  }
+}
---
+++ manifests/slurm_jobscripts.pp
@@ -0,0 +1,44 @@
+class trailblazing_turtle::slurm_jobscripts (
+  String $api_url,
+  String $token
+) {
+  ensure_packages(['python3', 'python3-requests'])
+  $slurm_jobscript_ini = @("EOT")
+    [slurm]
+    spool = /var/spool/slurm
+
+    [api]
+    host = ${api_url}
+    script_length = 100000
+    token = ${token}
+    | EOT
+
+  file { '/etc/slurm/slurm_jobscripts.ini':
+    ensure  => 'file',
+    owner   => 'slurm',
+    group   => 'slurm',
+    mode    => '0600',
+    notify  => Service['slurm_jobscripts'],
+    content => $slurm_jobscript_ini,
+  }
+
+  file { '/etc/systemd/system/slurm_jobscripts.service':
+    mode   => '0644',
+    source => 'puppet:///modules/profile/userportal/slurm_jobscripts.service',
+    notify => Service['slurm_jobscripts'],
+  }
+
+  $portal_version = lookup('profile::userportal::install_tarball::version')
+  file { '/opt/software/slurm/bin/slurm_jobscripts.py':
+    mode    => '0755',
+    source  => "https://raw.githubusercontent.com/guilbaults/TrailblazingTurtle/v${portal_version}/slurm_jobscripts/slurm_jobscripts.py",
+    notify  => Service['slurm_jobscripts'],
+    require => Package['slurm'],
+    replace => false, # avoid the file being replaced at every puppet transaction because its mtime as returned by GitHub has changed.
+  }
+
+  service { 'slurm_jobscripts':
+    ensure => 'running',
+    enable => true,
+  }
+}
