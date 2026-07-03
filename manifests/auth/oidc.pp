566a427912bc0b5e309727c9b8b805baf59b7ee8
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-07-03 14:14:25 -0400

    Add support for OIDC and OAuth2+JWT authentication.
    Depends on https://github.com/guilbaults/TrailblazingTurtle/pull/122

---
+++ manifests/auth/oidc.pp
@@ -0,0 +1,33 @@
+class metrix::auth::oidc (
+  String $authorization_endpoint,
+  String $token_endpoint,
+  String $client_id,
+  String $client_secret,
+  Array[String] $extra_scopes = [],
+) {
+
+  file_line { 'mozilla-django-oidc':
+    ensure => present,
+    path   => '/var/www/metrix/requirements.txt',
+    before => Uv::Venv['metrix_venv'],
+    line   => 'mozilla-django-oidc~=5.0.2',
+  }
+  file { '/var/www/metrix/userportal/settings/92-local_auth.py':
+    show_diff => false,
+    content   => epp('metrix/92-local_oidc.py',
+      {
+        'extra_scopes'           => $extra_scopes,
+        'authorization_endpoint' => $authorization_endpoint,
+        'token_endpoint'         => $token_endpoint,
+        'client_id'              => $client_id,
+        'client_secret'          => $client_secret,
+      }
+    ),
+    owner     => 'apache',
+    group     => 'apache',
+    mode      => '0600',
+    require   => Class['metrix::install'],
+    notify    => Service['metrix'],
+  }
+}
+
